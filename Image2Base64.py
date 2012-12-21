21019148cc3ce0f8f93a82b4c1064841f6a08e23
Timur R Mingaliev <timur@mingaliev.com>
2012-12-21 16:39:50 +0400

    Plugin

---
+++ Image2Base64.py
@@ -0,0 +1,50 @@
+#encoding=utf8
+import sublime, sublime_plugin
+import os, base64
+
+class Image2Base64(sublime_plugin.EventListener):
+    def on_load(self, view):
+        if view.file_name():
+            fileName, fileExtension = os.path.splitext(view.file_name())
+            if fileExtension in conv_extensions:
+                image = convert_image(view)
+                if image:
+                    view.run_command("i2b64_change", {"image": image})
+                    #view.run_command("i2b64_panel", {"image": image})
+                    
+
+class I2b64Change(sublime_plugin.TextCommand):
+    def run(self, edit, image):
+        view = self.view
+        view.replace(edit, sublime.Region(0, view.size()), image)
+        view.set_read_only(True)    
+
+class I2b64Panel(sublime_plugin.TextCommand):
+    def run(self, edit, image):
+        window = self.view.window()
+        self.image = image
+        print "window"
+        items = []
+        items.append("Copy base64 image to clipboard")
+        items.append("Don't copy base64 image to clipboard")
+        print items
+        window.show_quick_panel(items, self.on_done) 
+
+    def on_done(self, selected):
+        if selected == 0:
+            print self.image
+
+conv_extensions = [
+        ".jpg",
+        ".jpeg",
+        ".gif",
+        ".png",
+        ".bmp",
+        ".ico"
+    ]
+
+def convert_image(view):
+    with open(view.file_name(), "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string
+    return None
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-sublime-text-2-image2base64
+Image2Base64
 ===========================
 
-Open images as converted to base64
+#### [Sublime Text 2](http://www.sublimetext.com/2)
+
+## About
+This is a Sublime Text 2 plugin allowing you to open images as converted to base64.
+
+## Usage
+Just click on image file in sidebar and it will be opened as base64 code.
+**Never save images as base64 code.**
+
+## Install
+### Package Controll
+
+The easiest way to install this is with [Package Control](http://wbond.net/sublime\_packages/package\_control).
+
+ * If you just went and installed Package Control, you probably need to restart Sublime Text 2 before doing this next bit.
+ * Bring up the Command Palette (Command+Shift+p on OS X, Control+Shift+p on Linux/Windows).
+ * Select "Package Control: Install Package" (it'll take a few seconds)
+ * Select Image2Base64 when the list appears.
+
+Package Control will automatically keep Git up to date with the latest version.
+
+### Alternative
+Drop Image2Base64.py file to Sublime's user packages directory and restart Sublime Text 2.
+
+If you have some problems or improvements with it, contact me via GitHub.
